63ed5d86f67fd82f5bbc4a56f0ee61f85bd92504
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2026-05-04 22:59:41 +0200

    chore: add package entrypoint (src/mod.ts) and jsr exports

---
+++ src/mod.ts
@@ -0,0 +1,7 @@
+export { runEpisode, train } from "./core/trainer.ts"
+export { QLearningAgent } from "./algorithms/qlearning.ts"
+export { DQNAgent } from "./algorithms/dqn.ts"
+export { ReplayBuffer } from "./memory/replayBuffer.ts"
+export { EpsilonGreedyPolicy } from "./policies/epsilonGreedy.ts"
+export { SeededRNG } from "./utils/random.ts"
+export type { Transition } from "./types/transition.ts"
